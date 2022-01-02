ca9a777fe30dd866f22bf49b7aa73e4089199509
Maxim Slipenko <max.slipenko@gmail.com>
2022-01-02 16:43:39 +0300

    add build script

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,37 @@
+const htmlnano = require('htmlnano')
+
 module.exports = function (eleventyConfig) {
+  if (process.env.NODE_ENV === 'production') {
+    eleventyConfig.addTransform('html-min', (content, outputPath) => {
+      if (outputPath) {
+        const isHtml = outputPath.endsWith('.html')
+        if (isHtml) {
+          return htmlnano.process(content, {
+            collapseAttributeWhitespace: true,
+            collapseWhitespace: 'conservative',
+            deduplicateAttributeValues: false,
+            minifyCss: false,
+            minifyJs: true,
+            minifyJson: false,
+            minifySvg: false,
+            removeComments: 'safe',
+            removeEmptyAttributes: false,
+            removeAttributeQuotes: false,
+            removeRedundantAttributes: true,
+            sortAttributesWithLists: false,
+            removeOptionalTags: false,
+            collapseBooleanAttributes: true,
+            mergeStyles: false,
+            mergeScripts: false,
+            minifyUrls: false
+          }).then(result => result.html)
+        }
+      }
+
+      return content
+    })
+  }
+
   return {
     dir: {
       input: 'src',
